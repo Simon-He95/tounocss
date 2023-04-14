0fd2c92eaf377ddc4effb5c800db82376e034a89
henrikvilhelmberglund <henrikvilhelmberglund@gmail.com>
2023-04-14 23:45:05 +0200

    feat: :sparkles: add svelte to languages in .ts

--- src/index.ts
+++ src/index.ts
@@ -10,7 +10,7 @@ export function activate(context: vscode.ExtensionContext) {
   const { dark, light } = vscode.workspace.getConfiguration('to-unocss')
 
   const process = new CssToUnocssProcess()
-  const LANS = ['html', 'vue', 'swan', 'wxml', 'axml', 'css', 'wxss', 'acss', 'less', 'scss', 'sass', 'stylus', 'wxss', 'acss']
+  const LANS = ['html', 'vue', 'svelte', 'swan', 'wxml', 'axml', 'css', 'wxss', 'acss', 'less', 'scss', 'sass', 'stylus', 'wxss', 'acss']
   // style
   const style = Object.assign({
     dark: {
