ec1a9036e172782250a6459ff0f7cd5384ea77aa
Simon He <57086651+Simon-He95@users.noreply.github.com>
2023-04-18 12:32:55 +0800

    fix: match error

--- src/index.ts
+++ src/index.ts
@@ -101,7 +101,7 @@ export function activate(context: vscode.ExtensionContext) {
             // 可能存在多项，查找离range最近的
             if (lineText.indexOf(':') < 1)
               return
-            const wholeReg = new RegExp(`([\\w\\-]+\\s*:\\s)?([\\w\\-\\[\\(\\!]+)?${word}(:*\\s*[^";\\/>]+)?`, 'g')
+            const wholeReg = new RegExp(`([\\w\\-]+\\s*:\\s)?([\\w\\-\\[\\(\\!]+)?${word}(:*\\s*[^:";\\/>]+)?`, 'g')
             for (const match of lineText.matchAll(wholeReg)) {
               const { index } = match
               const pos = index! + match[0].indexOf(word)
