183033c170967a3ede2779db72cc2c38578a8992
Simon He <57086651+Simon-He95@users.noreply.github.com>
2023-05-08 13:02:38 +0800

    fix: 高亮内容中多余的空格

--- src/index.ts
+++ src/index.ts
@@ -26,6 +26,7 @@ export function activate(context: vscode.ExtensionContext) {
       textDecoration: 'underline',
       backgroundColor: 'rgba(255, 165, 0, 0.5)',
       color: '#ffffff',
+      borderRadius: '6px',
     }, light),
   }
   const decorationType = vscode.window.createTextEditorDecorationType(style)
@@ -101,7 +102,7 @@ export function activate(context: vscode.ExtensionContext) {
             // 可能存在多项，查找离range最近的
             if (lineText.indexOf(':') < 1)
               return
-            const wholeReg = new RegExp(`([\\w\\-]+\\s*:\\s)?([\\w\\-\\[\\(\\!]+)?${word}(:*\\s*[^:"}{\`,;\\/>]+)?`, 'g')
+            const wholeReg = new RegExp(`([\\w\\-]+\\s*:\\s)?([\\w\\-\\[\\(\\!]+)?${word}(:*\\s*[^:"}{\`,;\\/>\\s]+)?`, 'g')
             for (const match of lineText.matchAll(wholeReg)) {
               const { index } = match
               const pos = index! + match[0].indexOf(word)
