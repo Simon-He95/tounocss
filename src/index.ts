0fdc523d4db0a475a3c358e0e5c562ea3dbd14d3
Simon He <57086651+Simon-He95@users.noreply.github.com>
2023-07-17 09:39:41 +0800

    chore: update

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 import * as vscode from 'vscode'
-import { copyText, getConfiguration, message, registerCommand } from '@vscode-use/utils'
+import { addEventListener, copyText, getConfiguration, message, registerCommand } from '@vscode-use/utils'
 import { CssToUnocssProcess } from './process'
 import { LRUCache, getMultipedUnocssText, hasFile } from './utils'
 // 'use strict'
@@ -17,7 +17,7 @@ export async function activate(context: vscode.ExtensionContext) {
   const styleReg = /style="([^"]+)"/
   const { dark, light } = getConfiguration('to-unocss')
   const process = new CssToUnocssProcess()
-  const LANS = ['html', 'vue', 'svelte', 'solid', 'swan', 'react', 'js', 'ts', 'tsx', 'jsx', 'wxml', 'axml', 'css', 'wxss', 'acss', 'less', 'scss', 'sass', 'stylus', 'wxss', 'acss']
+  const LANS = ['html', 'vue', 'svelte', 'solid', 'swan', 'react', 'javascriptreact', 'typescriptreact', 'js', 'ts', 'tsx', 'jsx', 'wxml', 'axml', 'css', 'wxss', 'acss', 'less', 'scss', 'sass', 'stylus', 'wxss', 'acss']
   const md = new vscode.MarkdownString()
   md.isTrusted = true
   md.supportHtml = true
@@ -38,9 +38,9 @@ export async function activate(context: vscode.ExtensionContext) {
     }, light),
   }
   const decorationType = vscode.window.createTextEditorDecorationType(style)
-
+  const disposes = []
   // 注册ToUnocss命令
-  registerCommand('tounocss.ToUnocss', async () => {
+  disposes.push(registerCommand('tounocss.ToUnocss', async () => {
     const textEditor = vscode.window.activeTextEditor!
     const doc = textEditor.document
     const fileName = doc.fileName
@@ -56,10 +56,10 @@ export async function activate(context: vscode.ExtensionContext) {
     textEditor.edit((builder) => {
       builder.replace(selection, newSelection)
     })
-  })
+  }))
 
   // 注册InlineStyleToUnocss命令
-  const disposable = registerCommand('tounocss.InlineStyleToUnocss', async () => {
+  disposes.push(registerCommand('tounocss.InlineStyleToUnocss', async () => {
     const textEditor = vscode.window.activeTextEditor!
     const doc = textEditor.document
     let selection: vscode.Selection | vscode.Range = textEditor.selection
@@ -77,94 +77,92 @@ export async function activate(context: vscode.ExtensionContext) {
     textEditor.edit((builder) => {
       builder.replace(selection, newSelection)
     })
-  })
+  }))
 
   // copy
-  registerCommand('tounocss.copyAttr', () => {
+  disposes.push(registerCommand('tounocss.copyAttr', () => {
     copyText(copyAttr)
     message.info('copy successfully')
-  })
-  registerCommand('tounocss.copyClass', () => {
+  }))
+  disposes.push(registerCommand('tounocss.copyClass', () => {
     copyText(copyClass)
     message.info('copy successfully')
-  })
+  }))
 
   // 注册hover事件
-  setTimeout(() => {
-    vscode.languages.registerHoverProvider(LANS, {
-      provideHover(document, position) {
-        // 获取当前选中的文本范围
-        const editor = vscode.window.activeTextEditor
-        if (!editor)
-          return
-        // 移除样式
-        vscode.window.activeTextEditor?.setDecorations(decorationType, [])
-        const selection = editor.selection
-        const wordRange = new vscode.Range(selection.start, selection.end)
-        let selectedText = editor.document.getText(wordRange)
-        const realRangeMap: any = []
-        if (!selectedText) {
-          const range = document.getWordRangeAtPosition(position) as any
-          let word = document.getText(range)
-          const line = range.c.c
-          const lineNumber = position.line
-          const lineText = document.lineAt(lineNumber).text
-          const styleMatch = word.match(styleReg)
-          if (styleMatch) {
-            word = styleMatch[1]
-            const index = styleMatch.index!
-            realRangeMap.push({
-              content: styleMatch[0],
-              range: new vscode.Range(
-                new vscode.Position(line, index!),
-                new vscode.Position(line, index! + styleMatch[1].length),
-              ),
-            })
-          }
-          else {
-            // 可能存在多项，查找离range最近的
-            if (lineText.indexOf(':') < 1)
-              return
-            const wholeReg = new RegExp(`([\\w\\-]+\\s*:\\s)?([\\w\\-\\[\\(\\!]+)?${word}(:*\\s*[^:"}{\`;\\/>]+)?`, 'g')
-            for (const match of lineText.matchAll(wholeReg)) {
-              const { index } = match
-              const pos = index! + match[0].indexOf(word)
-              if (pos === range?.c?.e) {
-                word = match[0]
-                realRangeMap.push({
-                  content: match[0],
-                  range: new vscode.Range(
-                    new vscode.Position(line, index!),
-                    new vscode.Position(line, index! + match[0].length),
-                  ),
-                })
-                break
-              }
+  disposes.push(vscode.languages.registerHoverProvider(LANS, {
+    provideHover(document, position) {
+      // 获取当前选中的文本范围
+      const editor = vscode.window.activeTextEditor
+      if (!editor)
+        return
+      // 移除样式
+      vscode.window.activeTextEditor?.setDecorations(decorationType, [])
+      const selection = editor.selection
+      const wordRange = new vscode.Range(selection.start, selection.end)
+      let selectedText = editor.document.getText(wordRange)
+      const realRangeMap: any = []
+      if (!selectedText) {
+        const range = document.getWordRangeAtPosition(position) as any
+        let word = document.getText(range)
+        const line = range.c.c
+        const lineNumber = position.line
+        const lineText = document.lineAt(lineNumber).text
+        const styleMatch = word.match(styleReg)
+        if (styleMatch) {
+          word = styleMatch[1]
+          const index = styleMatch.index!
+          realRangeMap.push({
+            content: styleMatch[0],
+            range: new vscode.Range(
+              new vscode.Position(line, index!),
+              new vscode.Position(line, index! + styleMatch[1].length),
+            ),
+          })
+        }
+        else {
+          // 可能存在多项，查找离range最近的
+          if (lineText.indexOf(':') < 1)
+            return
+          const wholeReg = new RegExp(`([\\w\\-]+\\s*:\\s)?([\\w\\-\\[\\(\\!]+)?${word}(:*\\s*[^:"}{\`;\\/>]+)?`, 'g')
+          for (const match of lineText.matchAll(wholeReg)) {
+            const { index } = match
+            const pos = index! + match[0].indexOf(word)
+            if (pos === range?.c?.e) {
+              word = match[0]
+              realRangeMap.push({
+                content: match[0],
+                range: new vscode.Range(
+                  new vscode.Position(line, index!),
+                  new vscode.Position(line, index! + match[0].length),
+                ),
+              })
+              break
             }
           }
-          selectedText = word.replace(/'/g, '').trim()
         }
+        selectedText = word.replace(/'/g, '').trim()
+      }
 
-        // 获取当前选中的文本内容
-        if (!selectedText || !/[\w\-]+\s*:[^.]+/.test(selectedText))
-          return
-        if (cacheMap.has((selectedText)))
-          return setStyle(editor, realRangeMap, cacheMap.get(selectedText))
-        const selectedUnocssText = getMultipedUnocssText(selectedText)
-        if (!selectedUnocssText)
-          return
-        // 设置缓存
-        cacheMap.set(selectedText, selectedUnocssText)
+      // 获取当前选中的文本内容
+      if (!selectedText || !/[\w\-]+\s*:[^.]+/.test(selectedText))
+        return
+      if (cacheMap.has((selectedText)))
+        return setStyle(editor, realRangeMap, cacheMap.get(selectedText))
+      const selectedUnocssText = getMultipedUnocssText(selectedText)
+      if (!selectedUnocssText)
+        return
+      // 设置缓存
+      cacheMap.set(selectedText, selectedUnocssText)
 
-        return setStyle(editor, realRangeMap, selectedUnocssText)
-      },
-    })
-  }, 500)
+      return setStyle(editor, realRangeMap, selectedUnocssText)
+    },
+  }))
 
   // 监听编辑器选择内容变化的事件
-  vscode.window.onDidChangeTextEditorSelection(() => vscode.window.activeTextEditor?.setDecorations(decorationType, []))
+  disposes.push(addEventListener('text-change', () => vscode.window.activeTextEditor?.setDecorations(decorationType, [])))
 
-  context.subscriptions.push(disposable)
+  context.subscriptions.push(...disposes)
   function setStyle(editor: vscode.TextEditor, realRangeMap: any[], selectedUnocssText: string) {
     // 增加decorationType样式
     editor.edit(() => editor.setDecorations(decorationType, realRangeMap.map((item: any) => item.range)))
